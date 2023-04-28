f57ff314cf1156b48a9b93508fc1a6bd9116253a
Karthik <karthik97live@gmail.com>
2023-04-28 20:01:18 +1000

    Adding layout wrapper

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+// import custom components
+import Navbar from "./navbar";
+
+export default function Layout({ children }) {
+  // styles the main html tag
+  const styles = {
+    display: "flex",
+    flexDirection: "row"
+  };
+  return (
+    <>
+        <main>{children}</main>
+        <Navbar />
+    </>
+  );
+}
