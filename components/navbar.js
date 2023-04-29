7635ba5caf525c7901d402f11017d2bd630d9692
Karthik <karthik97live@gmail.com>
2023-04-29 18:23:50 +1000

    Reiterated navigation bar with new icons

--- components/navbar.js
+++ components/navbar.js
@@ -1,29 +1,50 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/components/navbar.module.scss";
 
 const path = [
-  { uid: 21, name: ' Home', id: 1, path: '/' },
-  { uid: 31, name: 'About', id: 2, path: 'about' },
-  { uid: 41, name: 'Work', id: 3, path: 'work' },
-  { uid: 51, name: 'Photography', id: 4, path: 'photography' },
-  { uid: 61, name: 'Resume', id: 5, path: 'resume' }
+  { uid: 21, name: " Home", id: 1, path: "/", imgpath: "../menu/home-icon.svg" },
+  { uid: 31, name: "About", id: 2, path: "about", imgpath: "../menu/about-icon.svg" },
+  { uid: 41, name: "Work", id: 3, path: "work", imgpath: "../menu/work-icon.svg" },
+  { uid: 51, name: "Photography", id: 4, path: "photography", imgpath: "../menu/camera-icon.svg" },
+  { uid: 61, name: "Resume", id: 5, path: "resume", imgpath: "../menu/resume-icon.svg" }
 ];
 
 export default function Navbar() {
+
+  const router = useRouter();
+
+  var routePath = (routePath, navPath) => {
+    const nameValue = 'active';
+    var activeVal;
+
+    if (!routePath.localeCompare(navPath)) {
+      activeVal = nameValue;
+    } else if (routePath !== '/') {
+      activeVal = (routePath === ('/' + navPath)) ? nameValue : '';
+    } else {
+      activeVal = '';
+    }
+
+    return activeVal
+  };
+
+
   return (
     <>
-      <nav className={styles.container}>
-          {path.map((value) => {
-            return (
-              <div className={`${styles.icon} ${styles.hov}`} key={value.uid}>
-                <Link href={value.path}>
-                  <div className={styles.img} aria-label={value.name} role="img"></div>
-                  <div className={styles.text}>{value.name}</div>
-                </Link>
-              </div>
+      <nav className={styles.menu}>
+        {path.map((value) => {
+          return (
+            <Link href={value.path} className={`${styles.link} ${routePath(router.pathname, value.path)}`} key={value.uid}>
+              <img aria-label={value.name} alt={value.name} src={value.imgpath} className={styles.icon} />
+              <span className={styles.label}>{value.path}</span>
+            </Link>
           );
-          })}
-      </nav>
+        })}
+      </nav >
     </>
   )
 }
+
+
+// {router.asPath == "/#about" ? "active" : ""}
--- pages/photography.js
+++ pages/photography.js
@@ -14,19 +14,19 @@ export default function Photography() {
       {/* Layout wayper in nextjs */}
       <Layout>
         <main className="main">
-          <div className="content">
+          <div className="container">
             <div>
-            <h1>Photography page - Some of my clicks</h1>
+              <h1>Photography page - Some of my clicks</h1>
 
-            <p>When I was 4 years old my grandfather gave me my first camera. He was an avid hobby photographer and collector of quirky Soviet-era cameras. The camera he gave me was a Kodak Pocket Instamatic which used this really funky cartridge-loaded 110 film. I took a lot of really bad photos.</p>
+              <p>When I was 4 years old my grandfather gave me my first camera. He was an avid hobby photographer and collector of quirky Soviet-era cameras. The camera he gave me was a Kodak Pocket Instamatic which used this really funky cartridge-loaded 110 film. I took a lot of really bad photos.</p>
 
-<h3>Looking around</h3>
-<p>Although I indeed took some terrible photos at first, I think having my own camera so early in life had a big impact on me. It made me conscious about my surroundings and perhaps even fed my life-long curiosity for the world around me. Looking up is fun and can reveal a whole new world, even in a place where you’ve been walking a hundred times.</p>
+              <h3>Looking around</h3>
+              <p>Although I indeed took some terrible photos at first, I think having my own camera so early in life had a big impact on me. It made me conscious about my surroundings and perhaps even fed my life-long curiosity for the world around me. Looking up is fun and can reveal a whole new world, even in a place where you’ve been walking a hundred times.</p>
 
-<h3>People & time</h3>
-<p>Really, at the core of what intrigues me about photography are people and time. Photography is the closest thing we’ve got to time travel I think. Not only can photography capture a moment in time, later to be recalled, but photography has this magical way of allowing the recorder—the photographer—to create a picture that paints a moment in time in the way you experienced it, the way you felt about that moment.
-Good photography to me tells some sort of story. It could be very obvious as in showing someone doing something or an important event, but often the “story” part is hidden and faint, the sort of story that you don’t yet have words for in your mind but you know so well. A feeling or a state of mind.</p>
-<h2>Pictures Coming soon.</h2>
+              <h3>People & time</h3>
+              <p>Really, at the core of what intrigues me about photography are people and time. Photography is the closest thing we’ve got to time travel I think. Not only can photography capture a moment in time, later to be recalled, but photography has this magical way of allowing the recorder—the photographer—to create a picture that paints a moment in time in the way you experienced it, the way you felt about that moment.
+                Good photography to me tells some sort of story. It could be very obvious as in showing someone doing something or an important event, but often the “story” part is hidden and faint, the sort of story that you don’t yet have words for in your mind but you know so well. A feeling or a state of mind.</p>
+              <h2>Pictures Coming soon.</h2>
             </div>
           </div>
         </main>
