08c9bffe7a1654a660dff84e4c70d00a7d40608f
Karthik <karthik97live@gmail.com>
2023-05-01 08:18:56 +1000

    Iterating photography content

--- pages/photography.js
+++ pages/photography.js
@@ -5,7 +5,7 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import Image from 'next/image'
-import styles from '@/styles/pages/about.module.scss'
+import styles from '@/styles/pages/photography.module.scss'
 
 export default function Photography() {
   return (
@@ -16,27 +16,27 @@ export default function Photography() {
       {/* Layout wayper in nextjs */}
       <Layout>
         <main className="main">
-          <div className="container">
-            <section className={`${styles.content}`}>
-              <Image
-                className="titleImage"
-                src="/title/photographer.png"
-                alt="Next.js Logo"
-                width={120}
-                height={120}
-                priority />
-              <h1>Photography page</h1>
-              <h2>Some of my curated clicks</h2>
+          <div className="content">
+            <Image
+              className="titleImage"
+              src="/title/photographer.png"
+              alt="Next.js Logo"
+              width={120}
+              height={120}
+              priority />
+            <h1>Photography</h1>
+            <h2>Some of my clicks</h2>
 
-              <p>When I was 4 years old my grandfather gave me my first camera. He was an avid hobby photographer and collector of quirky Soviet-era cameras. The camera he gave me was a Kodak Pocket Instamatic which used this really funky cartridge-loaded 110 film. I took a lot of really bad photos.</p>
+            <section className="container">
+              <div className="">
+                <p>When I was 4-5 years old my father gave me my first camera. It was a <a href="https://www.flickr.com/photos/95742794@N05/27370388280/in/pool-camerawiki/" target="_blank">Yashica automatic</a> and then grew to a <a href="http://camera-wiki.org/wiki/Yashica_Samurai_X3.0" target="_blank">Yasica SLR</a> which he taught me how to focus. He was an avid family portrait photographer who without any fail carried his camera when ever there was an event and trusted me with it. I took a lot of really bad photos which he developed when he goes from my hometown to UAE and then develop it from there and send back via airmail with his comments for my lessons on the back side of it.</p>
 
-              <h3>Looking around</h3>
-              <p>Although I indeed took some terrible photos at first, I think having my own camera so early in life had a big impact on me. It made me conscious about my surroundings and perhaps even fed my life-long curiosity for the world around me. Looking up is fun and can reveal a whole new world, even in a place where you’ve been walking a hundred times.</p>
+                <p>Although I indeed took some terrible photos at first, I think having my own camera so early in life had a big impact on me. It made me conscious about my surroundings and perhaps even fed my life-long curiosity for the world around me. Looking up is fun and can reveal a whole new world, even in a place where you’ve been walking a hundred times.</p>
 
-              <h3>People & time</h3>
-              <p>Really, at the core of what intrigues me about photography are people and time. Photography is the closest thing we’ve got to time travel I think. Not only can photography capture a moment in time, later to be recalled, but photography has this magical way of allowing the recorder—the photographer—to create a picture that paints a moment in time in the way you experienced it, the way you felt about that moment.
-                Good photography to me tells some sort of story. It could be very obvious as in showing someone doing something or an important event, but often the “story” part is hidden and faint, the sort of story that you don’t yet have words for in your mind but you know so well. A feeling or a state of mind.</p>
-              <h2>Pictures Coming soon.</h2>
+                <h3>People & time</h3>
+                <p>Really, at the core of what intrigues me about photography are people and time. Photography is the closest thing we’ve got to time travel I think. Not only can photography capture a moment in time, later to be recalled, but photography has this magical way of allowing the recorder—the photographer—to create a picture that paints a moment in time in the way you experienced it, the way you felt about that moment. Good photography to me tells some sort of story. It could be very obvious as in showing someone doing something or an important event, but often the “story” part is hidden and faint, the sort of story that you don’t yet have words for in your mind but you know so well. A feeling or a state of mind.</p>
+                <h3 className="align-center">Pictures Coming soon.</h3>
+              </div>
             </section>
           </div>
         </main>
