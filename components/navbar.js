73d454b35cb1baca22627d14a3193b0cd81f512c
Karthik <karthik97live@gmail.com>
2023-05-01 08:18:41 +1000

    Adding tooltip to the menu

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ import styles from "../styles/components/navbar.module.scss";
 const path = [
   { uid: 21, name: " Home", id: 1, path: "/", imgpath: "../menu/home-icon.svg" },
   { uid: 31, name: "About", id: 2, path: "about", imgpath: "../menu/about-icon.svg" },
-  { uid: 41, name: "Work", id: 3, path: "work", imgpath: "../menu/work-icon.svg" },
+  { uid: 41, name: "Design Work", id: 3, path: "work", imgpath: "../menu/work-icon.svg" },
   { uid: 51, name: "Photography", id: 4, path: "photography", imgpath: "../menu/camera-icon.svg" },
   { uid: 61, name: "Resume", id: 5, path: "resume", imgpath: "../menu/resume-icon.svg" }
 ];
@@ -36,8 +36,8 @@ export default function Navbar() {
         {path.map((value) => {
           return (
             <Link href={value.path} className={`${styles.link} ${routePath(router.pathname, value.path)}`} key={value.uid}>
+              <span className={styles.tooltip}>{value.name}</span>
               <img aria-label={value.name} alt={value.name} src={value.imgpath} className={styles.icon} />
-              <span className={styles.label}>{value.path}</span>
             </Link>
           );
         })}
