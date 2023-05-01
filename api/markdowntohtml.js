9de06334fb8aa9fbca40f58daa75625e7dabcd6c
Karthik <karthik97live@gmail.com>
2023-05-01 13:41:52 +1000

    Fixing build error with router mapping

--- api/markdowntohtml.js
+++ api/markdowntohtml.js
@@ -2,8 +2,6 @@ import remark from 'remark'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown) {
-  console.log(markdown)
-  debugger
   const result = await remark().use(html).process(markdown)
   return result.toString()
 }
